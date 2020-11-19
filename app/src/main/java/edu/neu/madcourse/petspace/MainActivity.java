1690bbb02e52b5a8bccc84afc35ce5523bfbb17a
Paul Cruz <cruz.p@husky.neu.edu>
2020-11-19 15:42:49 -0800

    commit main progress kc

--- app/src/main/java/edu/neu/madcourse/petspace/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/petspace/MainActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import edu.neu.madcourse.petspace.ui.login.ForgotPasswordActivity;
 import edu.neu.madcourse.petspace.ui.login.LoginActivity;
@@ -22,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     Button forgot_pass_btn, logout_btn;
     private FirebaseAuth mAuth;
     private View post_bar;
+    private Toolbar mToolbar;
+    private DatabaseReference UserRef, PostRef, LikesRef, CommentRef;
+    private String CurrentUserId;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +37,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         initializeViews();
-
-
-
+        // Display post bar upon loading.
         post_bar = new View(this);
         post_bar.findViewById(R.id.post_bar_included);
 
+        //instantiate Objects
+        mAuth = FirebaseAuth.getInstance();
+        CurrentUserId = mAuth.getCurrentUser().getUid();
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        PostRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+        LikesRef = FirebaseDatabase.getInstance().getReference().child("Likes");
 
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setActionBar(mToolbar);
+        getActionBar().setIcon(R.drawable.ic_baseline_reorder_24);
+        getActionBar().setTitle("PetSpace");
+
+        view = findViewById(R.id.post_bar_included);
+
+        // Hide keyboard upon loading main screen.
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        // Display bottom navigation bar upon loading.
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomAppBar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+
+
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
