020f645e7abebf9f3b9718a90459fea58cf982bf
Paul Cruz <cruz.p@husky.neu.edu>
2020-11-19 13:32:38 -0800

    BottomNavMenu kc

--- app/src/main/java/edu/neu/madcourse/petspace/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/petspace/MainActivity.java
@@ -2,10 +2,17 @@ package edu.neu.madcourse.petspace;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import edu.neu.madcourse.petspace.ui.login.ForgotPasswordActivity;
 import edu.neu.madcourse.petspace.ui.login.LoginActivity;
@@ -23,8 +30,37 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         initializeViews();
 
+
+
         post_bar = new View(this);
         post_bar.findViewById(R.id.post_bar_included);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomAppBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Chat:
+                        Toast.makeText(MainActivity.this, "Chat", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.Search:
+                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.Profile:
+                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.Settings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
 
         logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
