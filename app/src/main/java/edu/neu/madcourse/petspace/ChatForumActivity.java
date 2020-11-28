7524154296684591539f7f47670c73d400663140
Paul Cruz <cruz.p@husky.neu.edu>
2020-11-28 15:55:15 -0800

    Friend Resuest, Find Pet Friends, Profile, commit-kc-progress

---
+++ app/src/main/java/edu/neu/madcourse/petspace/ChatForumActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.petspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatForumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_forum);
+    }
+}
