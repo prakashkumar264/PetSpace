006f428c9e2e67f6b5d69e2d0cf2c7aad0deadad
Paul Cruz <cruz.p@husky.neu.edu>
2020-11-28 10:59:37 -0800

    find_friends

---
+++ app/src/main/java/edu/neu/madcourse/petspace/FindFriendsActivity.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.petspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+    }
+
+
+
+}
