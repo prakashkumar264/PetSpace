392ed706c436b0e05b38c4f46500edc640061f7c
Paul Cruz <cruz.p@husky.neu.edu>
2020-11-29 08:14:57 -0800

    Update About

--- app/src/main/java/edu/neu/madcourse/petspace/SettingsActivity.java
+++ app/src/main/java/edu/neu/madcourse/petspace/SettingsActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.petspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    /**
+     * Called when the user taps the 'Return to PetSpace' button.
+     * @param view the View object that was clicked
+     */
+    public void onClickCancel(View view) {
+        //Sending User back to MainActivity.
+        Intent loginIntent = new Intent(SettingsActivity.this, MainActivity.class);
+        startActivity(loginIntent);
+    }
+
 }
