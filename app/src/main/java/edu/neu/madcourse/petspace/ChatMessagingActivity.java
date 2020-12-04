5686b5c19ef2579baa1d0ba777af71c72b912c00
Paul Cruz <cruz.p@husky.neu.edu>
2020-12-03 22:12:44 -0800

    Chat

--- app/src/main/java/edu/neu/madcourse/petspace/ChatMessagingActivity.java
+++ app/src/main/java/edu/neu/madcourse/petspace/ChatMessagingActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.scaledrone.lib.Listener;
 import com.scaledrone.lib.Room;
 import com.scaledrone.lib.RoomListener;
@@ -29,6 +37,11 @@ public class ChatMessagingActivity extends AppCompatActivity implements RoomList
     private ChatMessageAdapter messageAdapter;
     private ListView messagesView;
 
+    FirebaseAuth mAuth;
+    private DatabaseReference Users_Ref;
+    private DatabaseReference username;
+    private StorageReference UserProfileImageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +52,11 @@ public class ChatMessagingActivity extends AppCompatActivity implements RoomList
         messagesView = (ListView) findViewById(R.id.messages_view);
         messagesView.setAdapter(messageAdapter);
 
-        MemberData data = new MemberData(getRandomName(), getRandomColor());
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        MemberData data = new MemberData(uid, getRandomColor());
 
         scaledrone = new Scaledrone(channelID, data);
         scaledrone.connect(new Listener() {
